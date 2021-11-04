736749dc0c8a7e968692df0d5ce83e5b5cacea6b
AipBorges <mpbds@uic.es>
2021-11-04 21:14:02 +0100

    first get is now working

--- server/routes/thingsRoutes.js
+++ server/routes/thingsRoutes.js
@@ -3,4 +3,26 @@ const Thing = require("../../database/models/things");
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  const things = await Thing.find();
+  res.json(things);
+});
+
+router.get("/thing/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const searchedThing = await Thing.findById(id);
+    if (searchedThing) {
+      res.json(searchedThing);
+    } else {
+      const error = new Error("Thing learned not found");
+      error.code = 404;
+      throw error;
+    }
+  } catch (error) {
+    error.code = 400;
+    next(error);
+  }
+});
+
 module.exports = router;
