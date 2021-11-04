49eeb0934ae0d8c8ced79e33908bb485eec4c5fb
AipBorges <mpbds@uic.es>
2021-11-04 19:03:33 +0100

    file structure has been setup with database index

---
+++ database/index.js
@@ -0,0 +1,11 @@
+const chalk = require("chalk");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_STRING, (error) => {
+  if (error) {
+    debug(chalk.red("No se ha podido iniciar la base de datos."));
+    debug(chalk.red(error.message));
+    return;
+  }
+  debug(chalk.blue("Conectado a la base de datos"));
+});
